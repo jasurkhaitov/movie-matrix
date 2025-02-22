f7684ecb5c85f7fd5f69e8bbd8e3f2a4b60a4c76
Jasur <fake49735@gmail.com>
2025-02-22 19:56:14 +0500

    required tools installed

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from './components/ui/button'
+import { ModeToggle } from './shared/mode-toggle'
+
+export default function App() {
+  return (
+    <div>
+      <p className='text-xl'>Salom</p>
+
+<ModeToggle/>
+
+<Button>Salom</Button>
+    </div>
+  )
+}
